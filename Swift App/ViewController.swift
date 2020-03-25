7fb0dad130749e42869e10cc69139d5f37c7ee85
Mohit <mohit.kumar5037@gmail.com>
2020-03-25 10:22:11 +0530

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Mohit on 25/03/20.
+//  Copyright © 2020 Practise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
